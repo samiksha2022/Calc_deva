e397f92defa50ad7da0d2ec2c04cca65479fa758
samiksha2022 <101919352+samiksha2022@users.noreply.github.com>
2022-03-28 15:31:24 +0530

    Add files via upload
    Multiplication operation is added in the application

---
+++ eg3.py
@@ -0,0 +1,16 @@
+num1 = 7.5
+num2 = 6.3
+
+
+sum = num1 + num2
+
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+sub = num1 - num2
+
+print('The subtraction {0} and {1} is {2}'.format(num1, num2, sub))
+
+mul = num1*num2
+
+print('Multiplication value {0} and {1} is {2}'.format(num1, num2, mul))
